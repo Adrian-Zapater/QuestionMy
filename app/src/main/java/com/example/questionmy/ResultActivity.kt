f07a7ff81c457dddbfc95819f32b83b2e0364e65
deim <deim@d506.lab.deim>
2022-03-16 09:40:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/questionmy/ResultActivity.kt
@@ -0,0 +1,14 @@
+package com.example.questionmy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        this.intent?.extras?.getString(QuestionActivity.KEY_SCORE, "5")
+        this.intent?.extras?.getLong(QuestionActivity.KEY_RESPONSE_TIME, 0)
+    }
+}
